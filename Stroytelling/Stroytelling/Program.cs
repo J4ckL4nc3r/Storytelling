c2bc57dd1ff40e7ad7483afeb566c5f5b4926581
J4ckL4nc3r <Stinnxx10@gmail.com>
2024-03-22 13:00:23 +0100

    add.commit

---
+++ Stroytelling/Stroytelling/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stroytelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
