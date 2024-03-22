e99cc05443fbc4cd294bb75573befec294d3c799
KVNundSo <kevinnituom1996.sae@t-online.de>
2024-03-22 13:25:20 +0100

    Create State Machine

---
+++ Stroytelling/Stroytelling/WC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroytelling
+{
+    internal class WC : IState
+    {
+        public void Update()
+        {
+            Console.WriteLine("Sleeping");
+        }
+        public IState Check()
+        {
+            if (Console.ReadLine() == "s")
+            {
+                return new HERE_NEW_ROOM();
+            }
+            return null;
+        }
+
+        public void StartState()
+        {
+            Console.WriteLine("zZzZ");
+        }
+        public void EndState()
+        {
+            Console.WriteLine("Waking up");
+        }
+    }
+}
